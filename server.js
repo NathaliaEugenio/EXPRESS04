6881782dd5943f4fd3d18ebe3ea369723ac00ca3
Nathalia Eugenio <eugenionathalia@gmail.com>
2024-04-11 14:54:06 -0300

    subir arquivos iniciais

---
+++ server.js
@@ -0,0 +1,18 @@
+//criar a infraestrutura básica
+
+const express = require('express');
+const mysql = require('mysql2');
+const mysql_config = require('./mysql_config');
+
+const app = express();
+
+app.listen(3000,()=>{
+    console.log("Servidor na área mano!")
+})
+
+const connection=mysql.createConnection(mysql_config);
+   
+
+//app.get('/',(req,res)=>{
+
+//})
